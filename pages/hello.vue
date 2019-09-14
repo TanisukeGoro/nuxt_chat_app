3bf9e1d95ec09efa9df015eb4a8e5e74201ce760
Okita <keisi1100525@outlook.jp>
2019-09-14 10:13:29 +0900

    🎉initial commit

---
+++ pages/hello.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-layout>
+        <v-flex class="text-center">
+            <div class="text-center">
+                <logo />
+                <vuetify-logo />
+            </div>
+            <blockquote class="blockquote">
+                &#8220;Hello Vue Nuxt&#8221;
+                <footer>
+                    <small>
+                        <em>&mdash;John Johnson</em>
+                    </small>
+                </footer>
+            </blockquote>
+        </v-flex>
+    </v-layout>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+    components: {
+        Logo,
+        VuetifyLogo
+    },
+    data() {
+        return {
+            count: 0
+        }
+    }
+}
+</script>
